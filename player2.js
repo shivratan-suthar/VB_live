e89808eb6871a00d7a26f248b49a1f1421beee57
Shiv Ratan Suthar <113790974+shivratan-suthar@users.noreply.github.com>
2026-07-04 05:22:39 +0530

    Add files via upload

--- player2.js
+++ player2.js
@@ -9,8 +9,7 @@ window.ROOM_ID = 'classroom-room-101';
 const EDUCATOR_PEER_ID = 'educator-' + window.ROOM_ID;
 const LEARNER_PEER_ID = 'learner-' + Math.floor(Math.random() * 100000);
 
-const liveSocket = io(API_URL);
-console.log(liveSocket, "is api visible for player");
+const liveSocket = io('http://13.233.118.249:3000');
 
 // Configure PeerJS with reliable Google STUN servers for network traversal
 const peer = new Peer(LEARNER_PEER_ID, {
